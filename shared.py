d187cbd4c3729cff975a6bfdaa45445597c4b82d
Kyle Guggenheim <kylegug@gmail.com>
2025-04-28 23:40:45 -0400

    updated csv path

--- shared.py
+++ shared.py
@@ -7,4 +7,4 @@ tips = pd.read_csv(app_dir / "tips.csv")
 
 
 # app_dir2 = Path(__file__).parent
-groupme = pd.read_json(app_dir / "message.json")
+groupme = pd.read_csv(app_dir / "exported_messages.csv")
